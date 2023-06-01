45463d09edb227bfb337798a46e183e7c831e538
= <=>
2023-06-01 22:30:42 +0100

    login and register working but need to sort routes

--- index.js
+++ index.js
@@ -12,33 +12,6 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  const header = req.get("Authorization");
-
-  if (!header) {
-    console.error("Missing Authorization header.");
-    res.set("WWW-Authenticate", "Bearer");
-    res.sendStatus(401);
-    return;
-  }
-
-  const [type, token] = header.split(" ");
-
-  if (type.toLowerCase() !== "bearer" || !token) {
-    console.error("Invalid token.");
-    res.sendStatus(401);
-    return;
-  }
-  try {
-    const user = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = user;
-    next();
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(401);
-  }
-});
-
 app.get("/", async (req, res, next) => {
   try {
     res.send("Welcome! \nPlease login");
